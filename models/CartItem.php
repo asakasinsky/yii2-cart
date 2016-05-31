33110e38f977f39c60983779ecd46f52da15029f
Vasily Asakasinsky <asakasinsky@gmail.com>
2016-05-31 15:02:50 +0300

    Update CartItem.php

--- models/CartItem.php
+++ models/CartItem.php
@@ -52,8 +52,6 @@ class CartItem extends ActiveRecord
                     ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
                     ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
                 ],
-                // if you're using datetime instead of UNIX timestamp:
-                // 'value' => new Expression('NOW()'),
             ],
         ];
     }
